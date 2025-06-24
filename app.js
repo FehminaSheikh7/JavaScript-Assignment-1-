1d17b3d7a5234bc154a0f4865dd68212c2546388
Fehmina Sheikh <fehminasheikh10@gmail.com>
2025-06-24 19:07:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+alert("Welcome to my website!");
+alert("Please Enter a valid password");
+alert("Welcome to JS Land...\n Happy Coading!");
+alert("Welcome to Js Land");
+alert("Happy Coading!");
+console.log("Hello...I can JS through my web browser's console");
